5396d53bd1348a67b4ac5ecfe9d9d397fa525b54
Andrei David <dandrei279@gmail.com>
2021-12-14 18:02:56 +0000

    Move queries to `database_utils` class
    Signed-off-by: Andrei David <dandrei279@gmail.com>

--- consts.php
+++ consts.php
@@ -1,58 +1,13 @@
 <?php
-// SQL Queries
-$sql_questionids_anytopic_anydifficulty = '
-        SELECT id from {question}
-        WHERE category = :categoryid AND hidden = 0 AND qtype != "topictagged" AND qtype != "random"
-    ';
+        $sql_question_answer_lastused = '
+                SELECT question.id "id", question.questiontext "question_text", GROUP_CONCAT(answers.answer) "answers", topictagged.lastused "last_used"
+                FROM {question} question
+                    JOIN {question_answers} answers
+                        ON answers.question = question.id
+                    JOIN {question_topictagged} topictagged
+                        ON question.id = topictagged.questionid
+                WHERE question.category = :categoryid
+                GROUP BY question.id;
+        ';
 
-$sql_questionids_anydifficulty = '
-        SELECT tag_instance.itemid
-        FROM {tag} tag
-            JOIN {tag_instance} tag_instance ON tag.id = tag_instance.tagid
-        WHERE strcmp(upper(tag_instance.itemtype), \'QUESTION\') = 0 AND strcmp(upper(tag.name), upper(:topic)) = 0
-        INTERSECT
-        SELECT question.id
-        FROM {tag_instance} tag_instance
-            JOIN {question} question ON question.id = tag_instance.itemid
-        WHERE question.category = :categoryid AND question.hidden = 0
-     ';
-
-$sql_questionids_anytopic = '
-        SELECT tag_instance.itemid
-        FROM {tag} tag
-            JOIN {tag_instance} tag_instance ON tag.id = tag_instance.tagid
-        WHERE strcmp(upper(tag_instance.itemtype), \'QUESTION\') = 0 AND strcmp(upper(tag.name), upper(:difficulty)) = 0
-        INTERSECT
-        SELECT question.id
-        FROM {tag_instance} tag_instance
-            JOIN {question} question ON question.id = tag_instance.itemid
-        WHERE question.category = :categoryid AND question.hidden = 0
-     ';
-
-$sql_questionids = '
-        SELECT tag_instance.itemid
-        FROM {tag} tag
-            JOIN {tag_instance} tag_instance ON tag.id = tag_instance.tagid
-        WHERE strcmp(upper(tag_instance.itemtype), \'QUESTION\') = 0 AND strcmp(upper(tag.name), upper(:difficulty)) = 0
-        INTERSECT
-        SELECT tag_instance.itemid
-        FROM {tag} tag
-            JOIN {tag_instance} tag_instance ON tag.id = tag_instance.tagid
-        WHERE strcmp(upper(tag_instance.itemtype), \'QUESTION\') = 0 AND strcmp(upper(tag.name), upper(:topic)) = 0
-        INTERSECT
-        SELECT question.id
-        FROM {tag_instance} tag_instance
-            JOIN {question} question ON question.id = tag_instance.itemid
-        WHERE question.category = :categoryid AND question.hidden = 0
-     ';
-$sql_question_answer_lastused = '
-        SELECT question.id "id", question.questiontext "question_text", GROUP_CONCAT(answers.answer) "answers", topictagged.lastused "last_used"
-        FROM {question} question
-            JOIN {question_answers} answers
-                ON answers.question = question.id
-            JOIN {question_topictagged} topictagged
-                ON question.id = topictagged.questionid
-        WHERE question.category = :categoryid
-        GROUP BY question.id;
-';
 ?>
--- utils.php
+++ utils.php
@@ -73,14 +73,53 @@ class database_utils {
     public function get_questions($topic, $difficulty, $categoryid) {
         global $DB;
 
-        global $sql_questionids_anytopic_anydifficulty;
-        global $sql_questionids_anydifficulty;
-        global $sql_questionids_anytopic;
-        global $sql_questionids;
+        // SQL Queries
+        $sql_questionids_anytopic_anydifficulty = '
+                SELECT id from {question}
+                WHERE category = :categoryid AND hidden = 0 AND qtype != "topictagged" AND qtype != "random"
+            ';
 
-        require_once('consts.php');
+        $sql_questionids_anydifficulty = '
+                SELECT tag_instance.itemid
+                FROM {tag} tag
+                    JOIN {tag_instance} tag_instance ON tag.id = tag_instance.tagid
+                WHERE strcmp(upper(tag_instance.itemtype), \'QUESTION\') = 0 AND strcmp(upper(tag.name), upper(:topic)) = 0
+                INTERSECT
+                SELECT question.id
+                FROM {tag_instance} tag_instance
+                    JOIN {question} question ON question.id = tag_instance.itemid
+                WHERE question.category = :categoryid AND question.hidden = 0
+             ';
 
-        // Actual Query
+        $sql_questionids_anytopic = '
+                SELECT tag_instance.itemid
+                FROM {tag} tag
+                    JOIN {tag_instance} tag_instance ON tag.id = tag_instance.tagid
+                WHERE strcmp(upper(tag_instance.itemtype), \'QUESTION\') = 0 AND strcmp(upper(tag.name), upper(:difficulty)) = 0
+                INTERSECT
+                SELECT question.id
+                FROM {tag_instance} tag_instance
+                    JOIN {question} question ON question.id = tag_instance.itemid
+                WHERE question.category = :categoryid AND question.hidden = 0
+             ';
+
+        $sql_questionids = '
+                SELECT tag_instance.itemid
+                FROM {tag} tag
+                    JOIN {tag_instance} tag_instance ON tag.id = tag_instance.tagid
+                WHERE strcmp(upper(tag_instance.itemtype), \'QUESTION\') = 0 AND strcmp(upper(tag.name), upper(:difficulty)) = 0
+                INTERSECT
+                SELECT tag_instance.itemid
+                FROM {tag} tag
+                    JOIN {tag_instance} tag_instance ON tag.id = tag_instance.tagid
+                WHERE strcmp(upper(tag_instance.itemtype), \'QUESTION\') = 0 AND strcmp(upper(tag.name), upper(:topic)) = 0
+                INTERSECT
+                SELECT question.id
+                FROM {tag_instance} tag_instance
+                    JOIN {question} question ON question.id = tag_instance.itemid
+                WHERE question.category = :categoryid AND question.hidden = 0
+             ';
+       // Actual Query
         // Treat the "Any topic" and "Any difficulty" options separately
         if ($difficulty == 'Any difficulty' && $topic == 'Any topic') {
             $query = $sql_questionids_anytopic_anydifficulty;
@@ -91,6 +130,7 @@ class database_utils {
         } else {
             $query = $sql_questionids;
         }
+
         $questionids = $DB->get_records_sql($query,
             ['topic' => $topic, 'difficulty' => $difficulty, 'categoryid' => $categoryid]);
 
