82f99b42d58bab76033fb1162eb318a43f3a2cf6
Jumarea Stefan <69107167+StefanJum@users.noreply.github.com>
2022-01-09 16:47:21 +0200

    Apply suggestions from code review
    Co-authored-by: Andrei David <dandrei279@gmail.com>

--- lang/ro/qtype_topictagged.php
+++ lang/ro/qtype_topictagged.php
@@ -15,11 +15,11 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'qtype_topictagged', language 'en', branch 'MOODLE_20_STABLE'
+ * Strings for component 'qtype_topictagged', language 'ro', branch 'MOODLE_20_STABLE'
  *
  * @package    qtype
  * @subpackage topictagged
- * @copyright  2021 Andrei David; Ștefan Jumărea
+ * @copyright  2022 Andrei David; Ștefan Jumărea
 
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -28,7 +28,7 @@ $string['pluginname_help'] = 'Selectează o întrebare pe baza dificultății ș
 $string['pluginname_link'] = 'question/type/topictagged';
 $string['pluginnameadding'] = 'Adaugă o întrebare';
 $string['pluginnameediting'] = 'Editează o întrebare';
-$string['pluginnamesummary'] = 'Selectează o întrebare din banca de întrebări cu dificultatea si topicul ' .
+$string['pluginnamesummary'] = 'Selectează o întrebare din banca de întrebări cu dificultatea și topicul ' .
     'dorite care nu a fost folosită recent.<br>' .
     '<p><font color=#F57542><b>Atenție:</b></font><br>' .
     '<font color=#A14A28>Nu adăugați acest tip de întrebare în banca de întrebări. Adăugați direct în quiz.</font></p><br>' .
@@ -39,7 +39,7 @@ $string['pluginnamesummary'] = 'Selectează o întrebare din banca de întrebăr
     '<p>Pentru mai multe informații consultați <a href="https://github.com/systems-cs-pub-ro/quiz-manager-moodle#readme">Documentația</a></p>';
 $string['setdifficulty'] = 'Alegeți dificultatea';
 $string['settags'] = 'Alegeți topicul';
-$string['settagsempty'] = 'Nu s-a selectat un topic';
+$string['settagsempty'] = 'Nu s-a selectat niciun topic';
 $string['noquestions'] = 'Nu au fost găsite întrebări cu dificultatea și topicul selectate. Nu au fost adăugate întrebări la quiz.';
 $string['fileformat_warning'] = '<font color=#F57542><b>Atenție: </b></font>'.
     '<font color=#A14A28>Fișierul <b>MXML</b> poate fi reimportat; fișierul <b>CSV</b> <u>nu</u> poate fi reimportat.</font>';
@@ -47,7 +47,7 @@ $string['fileformat_warning'] = '<font color=#F57542><b>Atenție: </b></font>'.
 $string['selectedby'] = '{$a->questionname} selected by {$a->randomname}';
 
 $string['update_button'] = 'Actualizare';
-$string['download_button'] = 'Descărcați';
+$string['download_button'] = 'Descărcare';
 $string['download_mode'] = 'Formatul fișierului';
 
 $string['download_header'] = 'Exportați întrebările';
@@ -60,8 +60,8 @@ $string['randomqname'] = 'Topic Tagged Question {$a}';
 $string['editsettings'] = 'Administrarea Întrebărilor de tipul Topic Tagged';
 
 $string['setdifficulty_help'] = 'Alegeți dificultatea întrebării din lista de mai jos.';
-$string['settags_help'] = 'Topicurile sunt taguri definite de utilizatori care sunt adăugate întrebărilor în banca de întrebări. Ele trebuie să își păstreze forma în care au fost scrise când au fost adăugate întrebărilor, sau nu se vor găsi întrebări care să respecte condițiile. Topicurile sunt case insensitive și se poate alege un singur topic pentru o întrebare.';
+$string['settags_help'] = 'Topicurile sunt taguri definite de utilizatori care sunt adăugate întrebărilor în banca de întrebări. Ele trebuie să își păstreze forma în care au fost scrise când au fost adăugate întrebărilor, altfel nu vor fi găsite întrebări care să respecte condițiile. Topicurile sunt case insensitive și se poate alege un singur topic pentru o întrebare.';
 $string['update_header_help'] = 'Actualizați baza de date locală pentru a include tagul `last_used` al întrebărilor din categoria selectată. Actualizarea TREBUIE făcută de fiecare dată după importarea unui nou set de întrebări.';
 $string['download'] = 'Exportați întrebările';
 $string['download_help'] = 'Pentru a exporta întrebările, selectați categoria din care vreți sa descărcați întrebările, apoi alegeți un format de fișier.<ol><li><b><i><a href=https://docs.moodle.org/310/en/Moodle_XML_format> MXML </a> (Recomandat)</i></b> poate fi ușor reimportat. </li><li><i>CSV(complex)</i> mai ușor de manipulat datele cât timp exsita o copie locală.<br>Fișierul CSV va conține doar întrebările folosite în cel puțin un quiz.<br>Consultați <a href=https://github.com/systems-cs-pub-ro/quiz-manager-moodle#readme> documentația </a> pentru mai multe informații.</li></ol>';
-$string['available_questoins'] = 'Numărul de întrebări valabile:';
+$string['available_questoins'] = 'Numărul de întrebări disponibile:';
